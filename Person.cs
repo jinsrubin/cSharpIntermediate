e0593834d42b871c69370b0040747b5a0518390f
jinsjob <39972022+jinsjob@users.noreply.github.com>
2021-06-02 22:36:32 +1200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cSharpIntermediate
+{
+    public class Person
+    {
+        public string Name;
+        public int Id;
+        public List<string> Orders;
+
+        public Person()
+        {
+            this.Orders = new List<string>();
+        }
+
+        public Person(int id) : this()
+        {
+            this.Id = id;
+        }
+        public void Introduce(string to)
+        {
+            
+            Console.WriteLine($"HI {to} I am {Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            Person person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace cSharpIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Jins");
+            person.Introduce("Rubin");
+        }
+    }
+}
